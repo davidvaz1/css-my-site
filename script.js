28b0125ee0c5f3881cdb0719bfa75141591ad285
davidvaz1 <74098173+davidvaz1@users.noreply.github.com>
2021-12-02 11:12:17 +0530

    created and updated script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+<script>
+    function trigger_submit() {
+      window.alert("Are you sure you want to submit your message?")
+    }
 
+    function closeWindow() {
+      tab = window.close();
+    }
+
+
+  </script>
